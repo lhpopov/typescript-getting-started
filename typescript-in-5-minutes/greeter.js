78bf02703c626fe2651b9ad29d03c25b43c54af2
Lyubomir Popov <lhpopov@yahoo.com>
2019-03-20 10:31:20 +0200

    Initializing Typescript repo.

---
+++ typescript-in-5-minutes/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return 'Person bace, ' + person;
+}
+var user = ['Batsov'];
+document.body.innerHTML = greeter();
---
+++ typescript-in-5-minutes/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+	return 'Person bace, ' + person;
+}
+
+let user = ['Batsov'];
+
+document.body.innerHTML = greeter();
